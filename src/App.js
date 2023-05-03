2f0f2794bf77fdf3e4d18361f21326395d4eeb01
Shubham Singh <shubham1326thakur@gmail.com>
2023-05-03 23:46:00 +0530

    noise tacker on 3 may 2023

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Mic from "./components/micIcon/Mic";
+import Navbar from "./components/navbar/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Mic/>
+    </div>
+  );
+}
+
+export default App;
